ca0a635aba94cded62cd7e05af794c9d10f71cd8
Maciej Korzeniewski <mck.korzeniewski@gmail.com>
2023-04-24 09:52:47 +0300

    fix: handle messages received in background on android

--- lib/android/app/src/main/java/com/wix/reactnativenotifications/fcm/FcmInstanceIdListenerService.java
+++ lib/android/app/src/main/java/com/wix/reactnativenotifications/fcm/FcmInstanceIdListenerService.java
@@ -1,11 +1,13 @@
 package com.wix.reactnativenotifications.fcm;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 import com.wix.reactnativenotifications.BuildConfig;
+import com.wix.reactnativenotifications.core.NotificationIntentAdapter;
 import com.wix.reactnativenotifications.core.notification.IPushNotification;
 import com.wix.reactnativenotifications.core.notification.PushNotification;
 
@@ -17,18 +19,33 @@ import static com.wix.reactnativenotifications.Defs.LOGTAG;
  * @author amitd
  */
 public class FcmInstanceIdListenerService extends FirebaseMessagingService {
+  @Override
+  public void onMessageReceived(RemoteMessage message) {
+    Bundle bundle = message.toIntent().getExtras();
+    if (BuildConfig.DEBUG) Log.d(LOGTAG, "New message from FCM: " + bundle);
 
-    @Override
-    public void onMessageReceived(RemoteMessage message){
-        Bundle bundle = message.toIntent().getExtras();
-        if(BuildConfig.DEBUG) Log.d(LOGTAG, "New message from FCM: " + bundle);
-
-        try {
-            final IPushNotification notification = PushNotification.get(getApplicationContext(), bundle);
-            notification.onReceived();
-        } catch (IPushNotification.InvalidNotificationException e) {
-            // An FCM message, yes - but not the kind we know how to work with.
-            if(BuildConfig.DEBUG) Log.v(LOGTAG, "FCM message handling aborted", e);
-        }
+    try {
+      final IPushNotification notification = PushNotification.get(getApplicationContext(), bundle);
+      notification.onReceived();
+    } catch (IPushNotification.InvalidNotificationException e) {
+      // An FCM message, yes - but not the kind we know how to work with.
+      if (BuildConfig.DEBUG) Log.v(LOGTAG, "FCM message handling aborted", e);
     }
+  }
+
+  @Override
+  public void handleIntent(Intent intent) {
+    if (BuildConfig.DEBUG) Log.d(LOGTAG, "New intent: " + intent);
+
+    try {
+      final Bundle notificationData = NotificationIntentAdapter.extractPendingNotificationDataFromIntent(intent);
+      final IPushNotification pushNotification = PushNotification.get(this, notificationData);
+      if (pushNotification != null) {
+        pushNotification.onReceived();
+      }
+    } catch (IPushNotification.InvalidNotificationException e) {
+      // An FCM message, yes - but not the kind we know how to work with.
+      if (BuildConfig.DEBUG) Log.v(LOGTAG, "FCM message handling aborted", e);
+    }
+  }
 }
